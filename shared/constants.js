224a2608508269430d77605358a62f869303a0bc
Ivan Hermilio Mera Ibarguen <129082143+1v4nz2023@users.noreply.github.com>
2026-06-06 18:54:18 -0500

    feat: change url bk and add a name validation

--- shared/constants.js
+++ shared/constants.js
@@ -1,5 +1,7 @@
-export const API_BASE_URL = 'http://localhost:5678/webhook';
+export const API_BASE_URL = 'https://n8n-fly-calm-night-1063.fly.dev/webhook';
 
 export const ENTERPRISE_URL = `${API_BASE_URL}/enterprise`;
 
 export const DATA_URL = '../data/site-data.json';
+
+
